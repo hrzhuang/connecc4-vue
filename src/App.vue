1ce5a4a8b08641e7a234e8ef3607baaecbd7143f
Ricky Zhuang <hr.zhuang@outlook.com>
2019-07-14 17:07:26 -0400

    Create page layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <Game />
+    <div id="app">
+        <h1>connecc4</h1>
+        <Game />
+    </div>
 </template>
 
 
@@ -18,6 +21,27 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap');
 
 body {
+    background-color: #cceeff;
     font-family: 'Source Sans Pro', sans-serif;
+
+    /* Centre game on screen */
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    height: 100vh;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+#app > h1 {
+    color: #1a1a1a;
+    font-size: 64px;
+    font-weight: normal;
+    margin: 0;
+    text-align: center;
 }
 </style>
